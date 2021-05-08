aa9b698a4e26c841a83b818aae99de816908aaf3
w40141 <w.40141@gmail.com>
2021-05-08 23:08:05 +0900

    edit

--- abc_200/c.py
+++ abc_200/c.py
@@ -4,17 +4,17 @@
 
 n = int(input())
 num_li = list(map(int, input().split()))
-org = [0 for i in range(200)]
+org = [0 for _ in range(200)]
 for n in num_li:
-    org[n%200] += 1
+    org[n % 200] += 1
 
 num = 0
 for o in org:
     if o:
-        num += o * (o-1) / 2
+        num += int(o * (o - 1) / 2)
 # for i in range(n):
 #     for j in range(i+1, n):
 #         a = num_li[i] - num_li[j]
 #         if a % 200 == 0:
 #             num += 1
-print(int(num))
+print(num)
