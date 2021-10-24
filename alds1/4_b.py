27f61a6883c7f7728ee91ce76ea08e268e82e35e
w40141 <w.40141@gmail.com>
2021-10-24 22:07:40 +0900

    edit

--- alds1/4_b.py
+++ alds1/4_b.py
@@ -5,18 +5,32 @@ q = int(input())
 t = list(map(int, input().split()))
 
 
+# def binary_search(searched_li, search_number):
+#     if searched_li == []:
+#         return False
+#     else:
+#         index = int(len(searched_li) / 2)
+#         index_number = searched_li[index]
+#         if index_number == search_number:
+#             return True
+#         elif index_number < search_number:
+#             return binary_search(searched_li[index + 1:], search_number)
+#         else:
+#             return binary_search(searched_li[:index], search_number)
+
+
 def binary_search(searched_li, search_number):
-    if searched_li == []:
-        return False
-    else:
-        index = int(len(searched_li) / 2)
-        index_number = searched_li[index]
-        if index_number == search_number:
+    li = list(searched_li)
+    while li != []:
+        index = int(len(li) / 2)
+        if li[index] == search_number:
             return True
-        elif index_number < search_number:
-            return binary_search(searched_li[index + 1:], search_number)
+            break
+        elif li[index] < search_number:
+            li = li[index + 1:]
         else:
-            return binary_search(searched_li[:index], search_number)
+            li = li[:index]
+    return False
 
 
 ans = sum([binary_search(s, i) for i in t])
