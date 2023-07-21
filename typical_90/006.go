a73084d187ffcfc981ccbb4e5907a96dd456a8a0
w40141 <w.40141@gmail.com>
2023-07-21 11:55:12 +0900

    edit

---
+++ typical_90/006.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+const LAST = 26
+
+func main() {
+	var n, k int
+	var s string
+	fmt.Scan(&n, &k)
+	fmt.Scan(&s)
+
+	var c [1000009][LAST]int
+	for i := 0; i < LAST; i++ {
+		c[n][i] = n
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		for j := 0; j < LAST; j++ {
+			if int(s[i]-'a') == j {
+				c[i][j] = i
+			} else {
+				c[i][j] = c[i+1][j]
+			}
+		}
+	}
+
+	answer := ""
+	current := 0
+	for i := 0; i < k; i++ {
+		for j := 0; j < LAST; j++ {
+			next := c[current][j]
+			maxPossibleLength := n - next + i
+			if maxPossibleLength >= k {
+				answer += string('a' + j)
+				current = next + 1
+				break
+			}
+		}
+	}
+	fmt.Println(answer)
+}
